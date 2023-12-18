a99effe17c25085b35aec54a1984e9ea6a97555d
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-18 12:37:19 -0700

    chore: add test with ping command

---
+++ src/commands/test/ping.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Replies with Pong! and this is a test command"),
+	async execute(interaction) {
+		// deferReply will make the bot think for a bit before reply
+		await interaction.reply('Pong! Calculating ping...');
+		const ping = Date.now() - interaction.createdTimestamp;
+		try {
+      await interaction.editReply(`Pong! Ping time is ${ping}ms.`);
+    } catch (error) {
+      await interaction.followUp(`Pong! Ping time is ${ping}ms.`); 
+    }
+	},
+};
