2a6e9a0527f3c223db9139822f6ca7351ef89c21
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-18 15:20:19 -0700

    add bot status (#2)
    * feat: add bot status
    
    * style: the files have been formatted

--- src/commands/test/ping.js
+++ src/commands/test/ping.js
@@ -6,12 +6,12 @@ module.exports = {
 		.setDescription("Replies with Pong! and this is a test command"),
 	async execute(interaction) {
 		// deferReply will make the bot think for a bit before reply
-		await interaction.reply('Pong! Calculating ping...');
+		await interaction.reply("Pong! Calculating ping...");
 		const ping = Date.now() - interaction.createdTimestamp;
 		try {
-      await interaction.editReply(`Pong! Ping time is ${ping}ms.`);
-    } catch (error) {
-      await interaction.followUp(`Pong! Ping time is ${ping}ms.`); 
-    }
+			await interaction.editReply(`Pong! Ping time is ${ping}ms.`);
+		} catch (error) {
+			await interaction.followUp(`Pong! Ping time is ${ping}ms.`);
+		}
 	},
 };
--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,28 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		console.log(`The bot ${client.user.tag} has logged in and online`);
+
+		const activities = [
+			"the weather api",
+			"the weather somewhere on the internet",
+			"weather forecasting",
+		];
+
+		setInterval(() => {
+			const status = activities[Math.floor(Math.random() * activities.length)];
+			client.user.setPresence({
+				activities: [
+					{
+						name: `${status}`,
+						type: ActivityType.Watching,
+					},
+				],
+				status: "dnd",
+			});
+		}, 10000);
 	},
 };
