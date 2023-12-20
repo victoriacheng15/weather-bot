e4411a7d8091de7acf924a96df6ada66bf6dd612
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-20 15:31:51 -0700

    refactor weather (#4)
    * chore: move weather.js to its own folder instead of test folder
    
    * refactor: extract getWeatherInfo, formatTime, getTodayDate, formatLocation fucntions out of weather.js
    
    * chore: remove unused varaibles
    
    * style: the files have been formatted

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 
 const { BOT_TOKEN } = process.env;
 
