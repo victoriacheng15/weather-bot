2d4fed6bfedf37653ad60b391f80ef98bfbab6a1
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-20 15:31:15 -0700

    style: the files have been formatted

--- src/commands/weather/weather.js
+++ src/commands/weather/weather.js
@@ -1,66 +1,75 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const {getWeatherInfo, formatTime, getTodayDate, formatLocation} = require("../../utils/fetchWeather")
+const {
+	getWeatherInfo,
+	formatTime,
+	getTodayDate,
+	formatLocation,
+} = require("../../utils/fetchWeather");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("weather")
-    .setDescription("Get the weather of a city")
-    .addStringOption((option) =>
-      option
-        .setName("city")
-        .setDescription("Enter the name of the city")
-        .setRequired(true)
-    )
-    .addStringOption(option => 
-      option
-        .setName("country-code")
-        .setDescription("Enter the country code of the city")
-        .setRequired(true)
-    ),
+	data: new SlashCommandBuilder()
+		.setName("weather")
+		.setDescription("Get the weather of a city")
+		.addStringOption((option) =>
+			option
+				.setName("city")
+				.setDescription("Enter the name of the city")
+				.setRequired(true),
+		)
+		.addStringOption((option) =>
+			option
+				.setName("country-code")
+				.setDescription("Enter the country code of the city")
+				.setRequired(true),
+		),
 
 	async execute(interaction) {
-    const city = interaction.options.getString("city");
-    const countryCode = interaction.options.getString("country-code");
-    const today = getTodayDate()
-    const info = await getWeatherInfo(city, countryCode)
-    const weatherDesc = `Display weather information for ${formatLocation({city, countryCode})} on ${today}`
+		const city = interaction.options.getString("city");
+		const countryCode = interaction.options.getString("country-code");
+		const today = getTodayDate();
+		const info = await getWeatherInfo(city, countryCode);
+		const weatherDesc = `Display weather information for ${formatLocation({
+			city,
+			countryCode,
+		})} on ${today}`;
 
-    const exampleEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
-    .setTitle("Weather Info")
-    .setDescription(weatherDesc)
-    .addFields(
-      {
-        name: "Temperature",
-        value: `${info.temp}c`,
-        inline: false,
-      }, {
-        name: "Min Temperature",
-        value: `${info.min_temp}c`,
-        inline: true,
-      }, {
-      
-        name: "Max Temperture",
-        value: `${info.max_temp}c`,
-        inline: true,
-      }, {
-        name: "Humidity",
-        value: `${info.humidity}%`,
-        inline: false,
-      },
-      {
-        name: "Sunraise",
-        value: `${formatTime(info.sunrise)}`,
-        inline: true,
-      },
-      {
-        name: "Sunset",
-        value: `${formatTime(info.sunset)}`,
-        inline: false,
-      },
-      
-    )
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x0099ff)
+			.setTitle("Weather Info")
+			.setDescription(weatherDesc)
+			.addFields(
+				{
+					name: "Temperature",
+					value: `${info.temp}c`,
+					inline: false,
+				},
+				{
+					name: "Min Temperature",
+					value: `${info.min_temp}c`,
+					inline: true,
+				},
+				{
+					name: "Max Temperture",
+					value: `${info.max_temp}c`,
+					inline: true,
+				},
+				{
+					name: "Humidity",
+					value: `${info.humidity}%`,
+					inline: false,
+				},
+				{
+					name: "Sunraise",
+					value: `${formatTime(info.sunrise)}`,
+					inline: true,
+				},
+				{
+					name: "Sunset",
+					value: `${formatTime(info.sunset)}`,
+					inline: false,
+				},
+			);
 
-    await interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
+		await interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
 	},
 };
--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,46 +1,44 @@
 require("dotenv").config();
 
 async function getWeatherInfo(city, country) {
-  const API_URL = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather"
-  const URL =`${API_URL}?city=${city}&country=${country}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": process.env.API_KEY,
-      "X-RapidAPI-Host": process.env.API_HOST,
-    },
-  };
+	const API_URL = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather";
+	const URL = `${API_URL}?city=${city}&country=${country}`;
+	const options = {
+		method: "GET",
+		headers: {
+			"X-RapidAPI-Key": process.env.API_KEY,
+			"X-RapidAPI-Host": process.env.API_HOST,
+		},
+	};
 
-  try {
-    const response = await fetch(URL, options);
-    const data = await response.json();
-    return data
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await fetch(URL, options);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 function formatTime(date) {
-  return new Date(date * 1000).toLocaleString().split(",").at(-1)
+	return new Date(date * 1000).toLocaleString().split(",").at(-1);
 }
 
 function getTodayDate() {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = (today.getMonth() + 1).toString().padStart(2, "0");
-  const day = today.getDate().toString().padStart(2, "0");
-  return `${year}-${month}-${day}`;
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = (today.getMonth() + 1).toString().padStart(2, "0");
+	const day = today.getDate().toString().padStart(2, "0");
+	return `${year}-${month}-${day}`;
 }
 
 function formatLocation({ city, countryCode }) {
-  const formats = {
-    city: city[0].toUpperCase() + city.slice(1),
-    countryCode: countryCode.toUpperCase(),
-  };
+	const formats = {
+		city: city[0].toUpperCase() + city.slice(1),
+		countryCode: countryCode.toUpperCase(),
+	};
 
-  return `${formats.city}, ${formats.countryCode}`;
+	return `${formats.city}, ${formats.countryCode}`;
 }
-
-
 
 module.exports = { getWeatherInfo, formatTime, getTodayDate, formatLocation };
