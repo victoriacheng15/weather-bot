dff6e5352b9891a76bdbeff32179721f9b488b10
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-01-04 08:41:56 -0700

    misc edit (#9)
    * chore: remove run_container.sh
    
    * chore: clarify message on which one is reply and send
    
    * chore: use meaningful variable for embed and false to ephemeral

--- src/events/client/hello.js
+++ src/events/client/hello.js
@@ -4,11 +4,11 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.content === "hello") {
-			message.reply("Hey there! from hello.js");
+			message.reply("Hey there! with reply");
 		}
 
 		if (message.content === "hey") {
-			await message.channel.send("hey there! without reply from hello.js");
+			await message.channel.send("hey there! without reply");
 		}
 	},
 };
