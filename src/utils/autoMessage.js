9f6df8b678faf1f600300a1c43ee27ffca995d7c
Victoria Cheng <victoria.cheng15@Gmail.com>
2024-01-04 08:38:07 -0700

    feat: add the ability to send auto message on weather on a specific time

--- src/utils/autoMessage.js
+++ src/utils/autoMessage.js
@@ -7,10 +7,9 @@ const {CHANNEL_ID, CITY, COUNTRY_CODE} = process.env;
 const time = "0 6 * * *"
 
 async function autoMessage(client) {
-  const info = await getWeatherInfo(CITY, COUNTRY_CODE)
-  const {temp, min_temp, max_temp, humidity} = info
+  const {temp, min_temp, max_temp, humidity} = await getWeatherInfo(CITY, COUNTRY_CODE)
 
-  const weatherMdg = `Feeling curious about the weather in ${CITY} today? ️ It's currently a cool ${temp}c, with a low of ${min_temp}c and a high of ${max_temp}c. Humidity's at ${humidity}%, so dress accordingly! `
+  const weatherMdg = `Feeling curious about the weather in ${CITY} today? ️ It's currently ${temp}c, with a low of ${min_temp}c and a high of ${max_temp}c. Humidity's at ${humidity}%, so dress accordingly! `
 
   schedule.scheduleJob(time, async () => {
     const channel = client.channels.cache.get(CHANNEL_ID);
