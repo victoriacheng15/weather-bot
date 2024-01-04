09ed9a562257f36f8eaf150dc93ed1b76c88c05a
Victoria Cheng <victoria.cheng15@Gmail.com>
2024-01-04 10:44:15 -0700

    feat: create env js to export all env variables

---
+++ src/utils/env.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const {
+	BOT_TOKEN,
+	CLIENT_ID,
+	CLIENT_SECRET,
+	GUILD_ID,
+	CHANNEL_ID,
+	API_KEY,
+	API_HOST,
+	CITY,
+	COUNTRY_CODE,
+} = process.env;
+
+module.exports = {
+	BOT_TOKEN,
+	CLIENT_ID,
+	CLIENT_SECRET,
+	GUILD_ID,
+	CHANNEL_ID,
+	API_KEY,
+	API_HOST,
+	CITY,
+	COUNTRY_CODE,
+};
